3ef771e8ec56f899f2bf415c31652ce7869bfa6f
MajidAbuRmila <majid@controlplane.com>
2023-01-13 22:37:53 +0200

    Added missing properties to domain

--- examples/resources/cpln_domain/resource.tf
+++ examples/resources/cpln_domain/resource.tf
@@ -7,4 +7,63 @@ resource "cpln_domain" "example" {
     terraform_generated = "true"
     example             = "true"
   }
+  
+  spec {
+    dns_mode         = "string"
+    gvc_link         = "string"
+    accept_all_hosts = "true"
+
+    ports {
+      number   = 443
+      protocol = "http"
+
+      routes {
+        prefix         = "string"
+        replace_prefix = "string"
+        workload_link  = "string"
+        port           = 80
+      }
+
+      cors {
+        allow_origins {
+          exact = "string"
+        }
+
+        allow_methods     = ["allow_method_1", "allow_method_2", "allow_method_3"]
+        allow_headers     = ["allow_header_1", "allow_header_2", "allow_header_3"]
+        max_age           = "24h"
+        allow_credentials = "true"
+      }
+
+      tls {
+        min_protocol_version = "string"
+        cipher_suites        = "string"
+
+        client_certificate {
+          secret_link = "string"
+        }
+
+        server_certificate {
+          secret_link = "string"
+        }
+      }
+    }
+
+    status {
+      end_points {
+        url           = "string"
+        workload_link = "string"
+      }
+
+      status  = "string"
+      warning = "string"
+
+      locations {
+        name               = "string"
+        certificate_status = "string"
+      }
+
+      fingerprint = "string"
+    }
+  }
 }
--- internal/provider/client/domain.go
+++ internal/provider/client/domain.go
@@ -10,6 +10,73 @@ type Domain struct {
 	Description *string                 `json:"description,omitempty"`
 	Tags        *map[string]interface{} `json:"tags,omitempty"`
 	Links       *[]Link                 `json:"links,omitempty"`
+	Spec        *DomainSpec             `json:"spec,omitempty"`
+	Status      *DomainStatus           `json:"status,omitempty"`
+}
+
+type DomainSpec struct {
+	DnsMode        *string           `json:"dnsMode,omitempty"` // Enum: "cname", "ns"
+	GvcLink        *string           `json:"gvcLink,omitempty"`
+	AcceptAllHosts *bool             `json:"acceptAllHosts,omitempty"`
+	Ports          *[]DomainSpecPort `json:"ports,omitempty"`
+}
+
+type DomainStatus struct {
+	EndPoints   *[]DomainEndPoint       `json:"endPoints,omitempty"`
+	Status      *string                 `json:"status,omitempty"` // Enum: "initializing", "ready", "pendingDnsConfig", "pendingCertificate", "usedByGvc"
+	Warning     *string                 `json:"warning,omitempty"`
+	Locations   *[]DomainStatusLocation `json:"locations,omitempty"`
+	Fingerprint *string                 `json:"fingerprint,omitempty"`
+}
+
+/*** Spec Related ***/
+type DomainSpecPort struct {
+	Number   *int           `json:"number,omitempty"`   // Maybe it should be a float because it is type number?
+	Protocol *string        `json:"protocol,omitempty"` // Enum: "http", "http2"
+	Routes   *[]DomainRoute `json:"routes,omitempty"`
+	Cors     *DomainCors    `json:"cors,omitempty"`
+	TLS      *DomainTLS     `json:"tls,omitempty"`
+}
+
+type DomainRoute struct {
+	Prefix        *string `json:"prefix,omitempty"`
+	ReplacePrefix *string `json:"replacePrefix,omitempty"`
+	WorkloadLink  *string `json:"workloadLink,omitempty"`
+	Port          *int    `json:"port,omitempty"`
+}
+
+type DomainCors struct {
+	AllowOrigins     *[]DomainAllowOrigin `json:"allowOrigins,omitempty"`
+	AllowMethods     *[]string            `json:"allowMethods,omitempty"`
+	AllowHeaders     *[]string            `json:"allowHeaders,omitempty"`
+	MaxAge           *string              `json:"maxAge,omitempty"`
+	AllowCredentials *bool                `json:"allowCredentials,omitempty"`
+}
+
+type DomainTLS struct {
+	MinProtocolVersion *string            `json:"minProtocolVersion,omitempty"` // Enum: "TLSV1_2", "TLSV1_1", "TLSV1_0"
+	CipherSuites       *[]string          `json:"cipherSuites,omitempty"`       // Enum: "ECDHE-ECDSA-AES256-GCM-SHA384", "ECDHE-ECDSA-CHACHA20-POLY1305", "ECDHE-ECDSA-AES128-GCM-SHA256", "ECDHE-RSA-AES256-GCM-SHA384", "ECDHE-RSA-CHACHA20-POLY1305", "ECDHE-RSA-AES128-GCM-SHA256", "AES256-GCM-SHA384", "AES128-GCM-SHA256"
+	ClientCertificate  *DomainCertificate `json:"clientCertificate,omitempty"`
+	ServerCertificate  *DomainCertificate `json:"serverCertificate,omitempty"`
+}
+
+type DomainAllowOrigin struct {
+	Exact *string `json:"exact,omitempty"`
+}
+
+type DomainCertificate struct {
+	SecretLink *string `json:"secretLink,omitempty"`
+}
+
+/*** Status Related ***/
+type DomainEndPoint struct {
+	URL          *string `json:"url,omitempty"`
+	WorkloadLink *string `json:"workloadLink,omitempty"`
+}
+
+type DomainStatusLocation struct {
+	Name              *string `json:"name,omitempty"`
+	CertificateStatus *string `json:"certificateStatus,omitempty"` // Enum: "initializing", "ready", "pendingDnsConfig", "pendingCertificate "
 }
 
 // GetDomain - Get Domain by name
