1931894268abfc3f59ea0f9edb884e77246e46f5
Majid Abu Rmelah <120715558+MajidAbuRmila@users.noreply.github.com>
2023-07-20 01:04:53 +0300

    Removed examples folder (#33)

--- examples/resources/cpln_audit_context/resource.tf
+++
@@ -1,9 +0,0 @@
-resource "cpln_audit_context" "example" {
-    name = "audit-context-example"
-    description = "audit context description" 
-    
-    tags = {
-        terraform_generated = "true"
-        example = "true"
-    }
-}
