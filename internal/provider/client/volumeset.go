3e397158535e28e56bd7563ba925af50aea19462
Majid Abu Rmila <120715558+MajidAbuRmila@users.noreply.github.com>
2024-02-07 03:40:33 +0200

    Add schedule to volume set (#59)
    * Add schedule to volume set
    
    * Add org resource (#55)
    
    * Add org resource
    
    * Update group docs
    
    * Update to v1.1.16
    
    * Add org resource
    
    * Update to v1.1.16
    
    * WIP - Updates to logic
    
    * WIP: Adding refresh token functionality
    
    * Finish org resource
    
    ---------
    
    Co-authored-by: Eric K <eric@controlplane.com>
    
    * Update workload test and dependencies
    
    * Add stderr to exec cpln command (#58)
    
    * Add stderr to exec cpln command
    
    * Add org resource (#55)
    
    * Add org resource
    
    * Update group docs
    
    * Update to v1.1.16
    
    * Add org resource
    
    * Update to v1.1.16
    
    * WIP - Updates to logic
    
    * WIP: Adding refresh token functionality
    
    * Finish org resource
    
    ---------
    
    Co-authored-by: Eric K <eric@controlplane.com>
    
    * Update workload test and dependencies
    
    * Add stderr to exec cpln command
    
    ---------
    
    Co-authored-by: Eric K <eric@controlplane.com>
    
    * Add binding id to volume set status
    
    * update docs
    
    * Updates noted by kyle
    
    * another doc fix
    
    * fix example
    
    * Add schedule to volume set
    
    * Add binding id to volume set status
    
    * update docs
    
    * Updates noted by kyle
    
    * another doc fix
    
    * fix example
    
    * rebase and up version
    
    ---------
    
    Co-authored-by: Eric K <eric@controlplane.com>

--- internal/provider/client/volumeset.go
+++ internal/provider/client/volumeset.go
@@ -20,12 +20,14 @@ type VolumeSetSpec struct {
 type VolumeSetStatus struct {
 	ParentID       *string        `json:"parentID,omitempty"`
 	UsedByWorkload *string        `json:"usedByWorkload,omitempty"`
+	BindingID      *string        `json:"bindingId,omitempty"`
 	Locations      *[]interface{} `json:"locations,omitempty"`
 }
 
 type VolumeSetSnapshots struct {
 	CreateFinalSnapshot *bool   `json:"createFinalSnapshot,omitempty"`
 	RetentionDuration   *string `json:"retentionDuration,omitempty"`
+	Schedule            *string `json:"schedule,omitempty"`
 }
 
 type VolumeSetScaling struct {
